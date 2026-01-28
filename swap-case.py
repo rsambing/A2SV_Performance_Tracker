78abeb6be53b08582cf4f1a390d7b33aeb554e46
Reinaldo Sambinga <reinaldogts252@gmail.com>
2026-01-28 10:49:50 +0100

    solved: swap case

---
+++ swap-case.py
@@ -0,0 +1,15 @@
+def swap_case(s):
+    ans = ""
+    for c in s:
+        if c.islower():
+            ans += c.upper()
+        elif c.upper():
+            ans += c.lower()
+        else:
+            ans += c
+    return ans
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
